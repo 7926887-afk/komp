38a9424955690897a0bfcbb5998097099d47b45f
Dmitry <7926887@gmail.com>
2026-05-29 17:44:42 +0300

    init11

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 @chcp 65001
 
 @rem формирование файла конфигурации. для включения раскомментируйте код ниже
-call vrunner compile --src src/cf --out build/1cv8.cf %*
+@remcall vrunner compile --src src/cf --out build/1cv8.cf %*
 
 @rem обновление конфигурации основной разработческой ИБ из хранилища. для включения раскомментируйте код ниже
 @rem call vrunner loadrepo %*
@@ -12,4 +12,4 @@ call vrunner compile --src src/cf --out build/1cv8.cf %*
 @rem call vrunner compileepf src/erf/МойВнешнийОтчет build %*
 
 @rem собрать расширения конфигурации внутри ИБ
-@rem call vrunner compileext src/cfe/МоеРасширение МоеРасширение %*
+call vrunner compileext src/cfe/osn osn %*
